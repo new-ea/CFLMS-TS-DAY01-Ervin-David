a563d1e35940fb79cdf98172b72168d468829bd8
Ervin Ademovic <newea1984@gmail.com>
2020-06-17 13:25:14 +0200

    finished Basic

--- Basic/Exercise2/script.js
+++ Basic/Exercise2/script.js
@@ -1,20 +1,42 @@
 "use strict";
 var myName = [{ name1: "Ernie" }, { name2: "Bert" }];
 var out = document.querySelector(".res");
+var res = document.querySelector(".out");
+var res2 = document.querySelector(".out2");
+var res3 = document.querySelector(".out3");
 myName.forEach(function (name, index) {
     var name1 = name["name1"];
     var name2 = name["name2"];
     for (var i = 0; i < 10; i++) {
         if (index == 0) {
-            document.querySelector(".res").innerHTML += name1 + "<br>";
+            out.innerHTML += name1 + "<br>";
         }
         else {
-            setTimeout(function () { return document.querySelector(".res").innerHTML += name2 + "<br>"; }, 5000);
+            setTimeout(function () { return out.innerHTML += name2 + "<br>"; }, 5000);
         }
     }
 });
 //////////////////////////////////////////////////////////////
 var myName2 = ["Ernie", "Bert", "Samson"];
 myName2.forEach(function (val) {
-    console.log(myName2[0]);
+    console.log(val);
+});
+/////////////////////////////////////////////////////////////
+var myName3 = ["Ernie", "Bert", "Samson", "Marie", "Jeff", "Susan", "Steve"];
+// 1: Print only the index of the elements.(ForIn)
+res.innerHTML = "<hr>";
+for (var index in myName3) {
+    res.innerHTML += index + "<br>";
+}
+;
+// 2. Print only the values. (ForOf)
+res2.innerHTML = "<hr>";
+for (var _i = 0, myName3_1 = myName3; _i < myName3_1.length; _i++) {
+    var value = myName3_1[_i];
+    res2.innerHTML += value + "<br>";
+}
+//  3. Print both index and values(ForIn)
+res3.innerHTML = "<hr>";
+myName3.forEach(function (name, index) {
+    res3.innerHTML += index + ":" + name + "<br>";
 });
--- Basic/Exercise2/script.ts
+++ Basic/Exercise2/script.ts
@@ -2,15 +2,18 @@
 
 let myName = [{name1:"Ernie"},{name2:"Bert"}]
 let out = document.querySelector(".res");
+let res = document.querySelector(".out");
+let res2 = document.querySelector(".out2");
+let res3 = document.querySelector(".out3");
 
 myName.forEach(function(name, index): any {
     let name1: string = name["name1"];
     let name2: string = name["name2"]; 
     for (let i: number = 0; i < 10; i++) {
         if (index == 0) {
-            document.querySelector(".res").innerHTML += name1 + "<br>";
+            out.innerHTML += name1 + "<br>";
         } else {
-            setTimeout(()=> document.querySelector(".res").innerHTML += name2 + "<br>", 5000);
+            setTimeout(()=> out.innerHTML += name2 + "<br>", 5000);
        }  
     }      
 });
@@ -19,12 +22,29 @@ myName.forEach(function(name, index): any {
 let myName2: Array<string> = ["Ernie", "Bert", "Samson"];
  
 myName2.forEach((val) => {
-    console.log(myName2[0]);  
+    console.log(val);  
 });
 
+/////////////////////////////////////////////////////////////
+let myName3: Array<string> = ["Ernie", "Bert", "Samson", "Marie", "Jeff", "Susan", "Steve"];
 
+// 1: Print only the index of the elements.(ForIn)
+res.innerHTML = "<hr>"
+for (let index in myName3) {
+    res.innerHTML += index + "<br>";
+};
 
+// 2. Print only the values. (ForOf)
+res2.innerHTML = "<hr>"
+for (let value of myName3) {
+    res2.innerHTML += value + "<br>";
+ }
 
+//  3. Print both index and values(ForIn)
+res3.innerHTML = "<hr>"
+myName3.forEach(function(name, index): any {
+    res3.innerHTML += index + ":" + name + "<br>";          
+});
 
 
 
